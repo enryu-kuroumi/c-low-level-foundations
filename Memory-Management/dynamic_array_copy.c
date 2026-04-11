f377d94afe4ef1d113d17809aac9096366aa16eb
Enryu <139391553+enryu-kuroumi@users.noreply.github.com>
2026-04-11 17:13:58 +0200

    Create dynamic_array_copy.c

---
+++ Memory-Management/dynamic_array_copy.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+int wyswietl(int rows, int cols, int a[rows][cols]) {
+    int w = 1; 
+
+   
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            a[i][j] = w++;
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%3d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%p ", &a[i][j]);
+        }
+        printf("\n");
+    }
+}
+int *copy(int rows, int cols, int a[rows][cols])
+{
+  int all = rows * cols;
+  int *copy = malloc(all * sizeof(int));
+  for (int i = 0; i < all; i++){
+    copy[i] = a[i/cols][i%cols];
+  }
+  int* tab2[rows];
+  for (int i = 0; i < rows; i++) {
+    tab2[i] = &copy[i * cols];
+  }
+
+  for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+          printf("%d ", tab2[i][j]);
+      }
+    printf("\n");
+  }
+  return copy;
+}
+void dealoc(int* arr2) {
+    free(arr2);
+}
+int main() {
+    int tab[2][2]; 
+    wyswietl(2, 2, tab);
+    copy(2, 2, tab);
+    return 0;
+}
